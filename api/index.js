087a3f569214cc6eddc93687d28378db04bbf759
ldeaatwork <143209681+ldeaatwork@users.noreply.github.com>
2026-02-11 22:08:40 +0700

    index.js

--- api/index.js
+++ api/index.js
@@ -44,7 +44,7 @@ async function handleEvent(event) {
   }
 
   try {
-    const model = genAI.getGenerativeModel({ model: "models/gemini-1.5-flash" });
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash-latest" });
     const result = await model.generateContent(prompt);
     const response = await result.response;
     return client.replyMessage(event.replyToken, { type: 'text', text: response.text() });
